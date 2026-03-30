8b976d7ce64df3f8f4ca3581ed46f5d990c085eb
ariogba patrick <obiscolmt@gmail.com>
2026-03-30 12:47:06 +0100

    hero section first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import Hero from './component/Hero'
 
 const App = () => {
   return (
+    <>
       <Navbar />
-    
+      <Hero />
+    </>  
   )
 }
 
